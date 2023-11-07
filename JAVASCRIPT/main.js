bb0e493ae6ce3d259f50d37815246afd9c442a2f
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2023-11-07 21:28:40 +0500

    commit

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,113 @@
+// // // const myHeading = document.querySelector("h1");
+// // // myHeading.textContent = "Hello world!";
+
+// // alert('Hello from JS!');
+// // let greetings="hi"
+// // "hi".toUpperCase();
+
+// // console.log("hi".toUpperCase());
+// // console.log("Hello world");
+// // console.log(3+4);
+// // console.log(1000/100+600-300*200);
+// // console.error("Oh No error showed up!");
+// // console.log("yeah this is a string", true, 1+6);
+// // const a=2;
+// // if(a===2){
+// //     console.log("its true");
+// // }
+// // else{
+// //  console.log("its false");
+// // }
+
+// // let rating=2;
+// // if(rating===2){
+// //  console.log("if() part runs");
+// // }
+// // else if(rating===1){
+// //  console.log("else() if part runs");
+// // }
+// // else{
+// //  console.log("HMM.. else part runs");
+// // }
+
+
+// // let shoppinList=["Lays", "potatoes" , "Eggs"];
+// // shoppinList[shoppinList.length]='Tomato';
+
+// // let password="hello kitty";
+// // if(password.length>=6){
+// //     if(password.indexOf(' ')===-1){
+// //         console.log('Valid Password!');
+// //     }
+// //     else{
+// //         console.error('Password is correct but does not contain black spaces!');
+// //     }
+// // }
+// // else{
+// //     console.log("Password must be longer.");
+// // }
+
+// // let loggedInUser;
+// // if(loggedInUser){   
+// //     console.log('YOU ARE LOGGED IN');
+// // }
+
+
+// // else{
+// //     console.log('PLEASE LOGIN')   
+// // }
+
+// // let shoppinList=['cheese', 'lemon','tomato', 'chips'];
+
+// // shoppinList[0]='milk';
+// // shoppinList[shoppinList.length]='Veges';
+// // shoppinList.length;
+ 
+// let topSongs= [
+//     'Hills',
+//     'Die for you',
+//     'Old town'
+// ]
+// //in order to add a song to the very last we use (push) method to use a value to be added!
+// topSongs.push('Save your tears');
+
+// //in order to remove the last song we use the method (pop)
+// topSongs.pop();
+
+
+// //in order to remove a value at the beginning or to the start we use the method (shift)
+// topSongs.shift();
+
+// // in ordr to add a value at the beginning we use the method (unshift)
+// topSongs.unshift('call out my name');
+
+// const arr1=[1,2,3,4];
+// const arr2=[5,6,7,8];
+// const arr3=[9,10,11,12];
+
+// console.log(arr1.concat(arr2,arr3));
+// //include() methods gives boolean value in return.[true or false]
+// arr1.includes(1); //this will give true in return because the value 1 is in the arr1
+// arr1.includes(9); //this gives false because value 9 is not in arr1.
+
+// if(arr1.includes(8)){
+//     console.log("YAY IT HAS THE VALUE FOUR AS EXPECTED!");
+// }
+// else
+// console.log('NAH! THIS VALUE DOESN\'T EXISTS');
+
+// const test=[
+//     'maths',
+//     'computer',
+//     'physics'
+
+// ]
+
+// if(test.includes('u')){
+//     console.log('Yayks Crap i am not good at maths');
+// }
+// // else{
+// //     console.log('OK');//if the value is not present it returns -1
+// // }
+
+let letters=['a','f','a','f'];
